5b3c20eea70dc7da274597d3f896d83bf1d11b76
Melissa Illig <melissaillig@melissas-mbp.home>
2020-05-19 15:46:30 +0200

    create JumpKey and SideKey

---
+++ Assets/MyGame/Scripts/WormController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public KeyCode jumpKey;
+    public KeyCode forwardKey;
+    public KeyCode backwardKey;
+    public Rigidbody z;
+    public Vector3 x;
+    public Vector3 y;
+    public Vector3 v;
+    public ForceMode f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(jumpKey))
+        {
+            //dieser Code wird ausgeführt sobald die in der Variable jumpKey definierte Taste gedrückt wird
+            Debug.Log("Sprungtaste wird gedrückt.");
+            z.AddForce(x, f);
+        }
+
+        if (Input.GetKeyDown(forwardKey))
+        {
+            Debug.Log("Charakter bewegt sich nach rechts.");
+            z.AddForce(y, f);
+        }
+
+        if (Input.GetKeyDown(backwardKey))
+        {
+            Debug.Log("Charakter bewegt sich nach links.");
+            z.AddForce(v, f);
+        }
+
+    }
+}
